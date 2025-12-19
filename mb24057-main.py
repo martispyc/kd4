bc1586cf80b25a167919c3c0333c4ac7553ab37c
Matiss Burmistrs <burmistrs3@gmail.com>
2025-12-19 20:09:32 +0200

    commit

--- mb24057-main.py
+++ mb24057-main.py
@@ -1,7 +1,7 @@
 # This is a simple calculation program
 # created to demonstrate version control.
-from libadd import *
-from libmult import *
+from mb24057libadd import *
+from mb24057libmult import *
 a = 13
 b = 5
 print(plus(a,b))
